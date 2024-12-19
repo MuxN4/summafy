e695f6be1677f8e289055640be109993eb5b4abc
MuxN4 <stev3npy@gmail.com>
2024-12-19 14:39:46 +0300

    integrate RTK Query for article summarization with RapidAPI

---
+++ src/services/article.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articlApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('x-rapidApi-key', rapidApiKey);
+            headers.set('x-rapidApi-host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+    }),
+    endpoints: (builder) => ({
+        getSummary: builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=4`
+        })
+    })
+});
+
+export const{ useLazyGetSummaryQuery } = articlApi;
